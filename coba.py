83e986e1e02ca497613989b5dff9122a12340452
indahdeshinta <indahitin@gmail.com>
2024-07-19 13:53:56 +0700

    first commit

---
+++ coba.py
@@ -0,0 +1,9 @@
+print("Hello, World!")
+#print("Hello, World!")
+print("Cheers, Mate!")
+x = 4       # x is of type int
+x = "Sally" # x is now of type str
+print(x)
+
+buah = ["apple", "banana", "cherry"]
+print(buah)
